79ede2dfc98626fb71ed1db4a7c2dae6d1f8f454
Jonathan Castellanos <Jonathan Castellanos>
2016-07-01 21:42:11 -0700

    added game_loop

--- src/main.py
+++ src/main.py
@@ -17,46 +17,49 @@ spriteImg = pygame.image.load(r"C:\Users\jdcas\eclipse\workspace\game1\sprites\b
 def sprite(x,y):
     gameDisplay.blit(spriteImg,(x,y))
 
-x_position = 300
-y_position = 250
-
-x_change = 0
-y_change = 0
-
-
-#-------------------------------------GAME LOOP---------------------------------------------
-crashed = False
-
-while not crashed:
-    for event in pygame.event.get():
-        
-        if event.type == pygame.QUIT:
-            crashed = True
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            elif event.key == pygame.K_RIGHT:
-                x_change = 5
-            elif event.key == pygame.K_UP:
-                y_change = -5
-            elif event.key == pygame.K_DOWN:
-                y_change = 5
-        
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
-                x_change = 0
-            if event.key == pygame.K_UP or pygame.K_DOWN:
-                y_change = 0
+def game_loop():
+    x_position = 300
+    y_position = 250
     
-    x_position += x_change
-    y_position += y_change
+    x_change = 0
+    y_change = 0
     
-    gameDisplay.fill(white)
-    sprite(x_position, y_position)
     
-    pygame.display.update()
-    clock.tick(60)
+    #-------------------------------------GAME LOOP---------------------------------------------
+    gameExit = False
+    
+    while not gameExit:
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                gameExit = True
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                elif event.key == pygame.K_RIGHT:
+                    x_change = 5
+                elif event.key == pygame.K_UP:
+                    y_change = -5
+                elif event.key == pygame.K_DOWN:
+                    y_change = 5
+            
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                    x_change = 0
+                if event.key == pygame.K_UP or pygame.K_DOWN:
+                    y_change = 0
+        
+        x_position += x_change
+        y_position += y_change
+        
+        gameDisplay.fill(white)
+        sprite(x_position, y_position)
+        
+        pygame.display.update()
+        clock.tick(60)
 #-------------------------------------------------------------------------------------------  
+
+game_loop()
 pygame.quit()
 quit()
