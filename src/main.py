b037b5fe100004b13d883b6349ade4f9b343307e
Jonathan Castellanos <Jonathan Castellanos>
2016-07-01 21:22:39 -0700

    added up and down movement

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ x_position = 300
 y_position = 250
 
 x_change = 0
+y_change = 0
 
 
 #-------------------------------------GAME LOOP---------------------------------------------
@@ -37,12 +38,19 @@ while not crashed:
                 x_change = -5
             elif event.key == pygame.K_RIGHT:
                 x_change = 5
-                
+            elif event.key == pygame.K_UP:
+                y_change = -5
+            elif event.key == pygame.K_DOWN:
+                y_change = 5
+        
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or pygame.K_RIGHT:
                 x_change = 0
+            if event.key == pygame.K_UP or pygame.K_DOWN:
+                y_change = 0
     
     x_position += x_change
+    y_position += y_change
     
     gameDisplay.fill(white)
     sprite(x_position, y_position)
